8bcaec34052aa901615bd92668880e05fffd51c0
Adel <moussaadel97@gmail.com>
2022-11-06 12:46:20 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from utils import *
+import pywintypes
+from win10toast import ToastNotifier
+import os
+import time
+
+email_list = ["moussaadel97@gmail.com"]
+
+toast = ToastNotifier()
+toast.show_toast("email_notifier", "The process has been started", duration=10)
+os.chdir(r"C:\Users\mouss\Desktop\root\personal_projects\scripts\email_notifier")
+
+
+def main():
+    time.sleep(60)
+
+    # unila
+    news = scrape_unila_news()
+    latest_news = news[0]
+
+    # pti
+    editais = scrape_pti_editais()
+    latest_edital = editais[0]
+
+    unila_update, pti_update = check_updates(latest_news[0], latest_edital[0])
+    write_to_json(latest_news[0], latest_edital[0])
+
+    if unila_update:
+        email_alert("Unila News", str(latest_news), email_list)
+
+    if pti_update:
+        email_alert("PTI Editais", str(latest_edital), email_list)
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,104 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from email.message import EmailMessage
+import json
+import pw
+import urllib.request
+
+
+def connected(host='http://google.com'):
+    try:
+        urllib.request.urlopen(host)  # Python 3.x
+        return True
+    except:
+        return False
+
+
+# sends email
+def email_alert(subject, body, to):
+    msg = EmailMessage()
+    msg.set_content(body)
+
+    # Gmail API key
+    user = pw.user
+    password = pw.pw
+
+    msg['from'] = user
+    msg['subject'] = subject
+    msg['to'] = to
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user, password)
+    server.send_message(msg)
+
+    server.quit()
+
+
+# Scrapes unila news webpage, returns news list with news[i] = [date_text, title_text, description_text, link]
+def scrape_unila_news():
+    news_list = []
+    r = requests.get("https://portal.unila.edu.br/noticias")
+    html_text = r.text
+    soup = BeautifulSoup(html_text, "lxml")
+
+    for article in soup.find_all('article', class_='tileItem'):
+        title = article.find('a', class_='summary url')
+        title_text = title.text
+        description_text = article.find('span', class_="description").text
+        date_text = article.find('span', class_="summary-view-icon").text.lstrip().rstrip()
+        link = title['href']
+
+        noticia = [date_text, title_text, description_text, link]
+        news_list.append(noticia)
+    return news_list
+
+
+# Scrapes PTI editais webpage, returns editales list with editales[i] =  [title_text, description_text]
+def scrape_pti_editais():
+    editais_list = []
+    r = requests.get("https://www.pti.org.br/editais-bolsas/")
+    html_text = r.text
+    soup = BeautifulSoup(html_text, "lxml")
+
+    for article in soup.find_all('div', class_='elementor-toggle-item'):
+        title = article.find('a', class_='elementor-toggle-title')
+        title_text = title.text
+        description_text = article.find('p').text
+
+        edital = [title_text, description_text]
+        editais_list.append(edital)
+    return editais_list
+
+
+def write_to_json(unila, pti):
+    dictionary = {
+        "unila": str(unila),
+        "pti": str(pti)
+    }
+
+    # Serializing json
+    json_object = json.dumps(dictionary, indent=4)
+
+    # Writing to sample.json
+    with open("latest.json", "w") as outfile:
+        outfile.write(json_object)
+
+
+def check_updates(new_unila, new_pti):
+    with open('latest.json', 'r') as openfile:
+        # Reading from json file
+        latest_json = json.load(openfile)
+
+        if latest_json['unila'] != new_unila:
+            unila_update = True
+        else:
+            unila_update = False
+
+        if latest_json['pti'] != new_pti:
+            pti_update = True
+        else:
+            pti_update = False
+
+        return unila_update, pti_update
